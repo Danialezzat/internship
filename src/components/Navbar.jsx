b89144d329338f336d0f4cb2ba7ffdf4cb6dc718
danial <the_timos@yahoo.com>
2025-03-31 22:56:26 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Userpic from '../assets/face-portrate.jpg'
 
 const Navbar = () => {
   return (
-    <section className='flex flex-row justify-between   p-2  w-[90%] h-24 bg-slate-500 m-auto mt-5 r-5 rounded-md'>
+    <section className='flex flex-row justify-between items-center    p-2  w-[90%] h-24 bg-slate-500 m-auto mt-5 r-5 rounded-md'>
       <div className='w-[40%] flex'>
         <img src={Userpic} alt="user" className='w-20 rounded-md'/>
         <div className='ml-5 font-bold flex flex-col justify-between  text-gray-200'>
@@ -15,8 +15,8 @@ const Navbar = () => {
       <div className='flex'>
         
         
-        <Link class="flex flex-row justify-center font-bold rounded-md bg-gradient-to-tr from-slate-800 to-slate-700 py-2 px-4 border border-transparent text-center text-xl text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" to="problemi"><button  type="button">Problemi</button></Link> 
-        <Link class=" ml-2 flex flex-row justify-center font-bold rounded-md bg-gradient-to-tr from-slate-800 to-slate-700 py-2 px-4 border border-transparent text-center text-xl text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" to="NuovoProblema "><button  type="button">Nuovo Problema</button></Link> 
+        <Link class="h-[80%] flex  flex-row  justify-center font-bold rounded-md bg-gradient-to-tr from-slate-800 to-slate-700 py-2 px-4 border border-transparent text-center text-l text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" to="problemi"><button  type="button">Problemi</button></Link> 
+        <Link class=" ml-2 h-[80%] flex flex-row   justify-center font-bold rounded-md bg-gradient-to-tr from-slate-800 to-slate-700 py-2 px-4 border border-transparent text-center text-l text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" to="NuovoProblema "><button  type="button">Nuovo Problema</button></Link> 
         
       </div>
 
--- src/components/Problemi.jsx
+++ src/components/Problemi.jsx
@@ -9,24 +9,58 @@ const data = [
 
 const Problemi = () => {
   return (
-    <div className="text-black">
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Gender</th>
-                </tr>
-                {data.map((val, key) => {
-                    return (
-                        <tr key={key}>
-                            <td>{val.name}</td>
-                            <td>{val.age}</td>
-                            <td>{val.gender}</td>
-                        </tr>
-                    )
-                })}
+        <div class="shadow-lg w-[90%] m-auto rounded-lg overflow-hidden mt-10  ">
+            <table class="w-full table-fixed">
+                <thead>
+                    <tr class="bg-gray-100">
+                        <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Name</th>
+                        <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Email</th>
+                        <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Category</th>
+                        <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Status</th>
+                        <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Edit/show</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white">
+                    <tr>
+                        <td class="py-4 px-6 border-b border-gray-200">John Doe</td>
+                        <td class="py-4 px-6 border-b border-gray-200 truncate">johndoe@gmail.com</td>
+                        <td class="py-4 px-6 border-b border-gray-200">Category</td>
+                        <td class="py-4 px-6 border-b border-gray-200">New</td>
+                        <td class="py-4 px-6 border-b border-gray-200"><button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 focus:outline-none focus:bg-blue-200 active:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:hover:bg-blue-900 dark:focus:bg-blue-900 dark:active:bg-blue-900">
+                        Edit/show
+                        </button></td>
+                    </tr>
+                    <tr>
+                        <td class="py-4 px-6 border-b border-gray-200">Jane Doe</td>
+                        <td class="py-4 px-6 border-b border-gray-200 truncate">janedoe@gmail.com</td>
+                        <td class="py-4 px-6 border-b border-gray-200">Category</td>
+                        <td class="py-4 px-6 border-b border-gray-200">New</td>
+                        <td class="py-4 px-6 border-b border-gray-200"><button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 focus:outline-none focus:bg-blue-200 active:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:hover:bg-blue-900 dark:focus:bg-blue-900 dark:active:bg-blue-900">
+                        Edit/show
+                        </button></td>
+                    </tr>
+                    <tr>
+                        <td class="py-4 px-6 border-b border-gray-200">Jane Doe</td>
+                        <td class="py-4 px-6 border-b border-gray-200 truncate">janedoe@gmail.com</td>
+                        <td class="py-4 px-6 border-b border-gray-200">category</td>
+                        <td class="py-4 px-6 border-b border-gray-200">In Progress</td>
+                        <td class="py-4 px-6 border-b border-gray-200"><button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 focus:outline-none focus:bg-blue-200 active:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:hover:bg-blue-900 dark:focus:bg-blue-900 dark:active:bg-blue-900">
+                        Edit/show
+                        </button></td>
+                    </tr>
+                    <tr>
+                        <td class="py-4 px-6 border-b border-gray-200">Jane Doe</td>
+                        <td class="py-4 px-6 border-b border-gray-200 truncate">janedoe@gmail.com</td>
+                        <td class="py-4 px-6 border-b border-gray-200">Category</td>
+                        <td class="py-4 px-6 border-b border-gray-200">Executed</td>
+                        <td class="py-4 px-6 border-b border-gray-200"><button type="button" class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 focus:outline-none focus:bg-blue-200 active:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:hover:bg-blue-900 dark:focus:bg-blue-900 dark:active:bg-blue-900">
+                        Edit/show
+                        </button></td>  
+                    </tr>
+                </tbody>
             </table>
         </div>
+        
   )
 }
 
